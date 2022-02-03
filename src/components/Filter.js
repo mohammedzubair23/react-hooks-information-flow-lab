23661bf16b7680d8535abeffa06ecb1a0586cf7b
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-02-03 14:49:46 -0500

    commit tree d39ef0d8bce0973c03924f5e3896d7ca7357e592

---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+
+function Filter({ selectedCategory, onCategoryChange }) {
+
+        return (
+            <select name="filter" onChange={onCategoryChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+             </select>
+        )
+}
+
+    export default Filter;
